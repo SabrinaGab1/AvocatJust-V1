9d7e9a96398f15d0f939a43d23e9eb0e4b6ce016
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:25:11 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,8 +4,4 @@
                  className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
                >
                 Vous êtes avocat ?
-                Vous êtes avocat ?
-                Vous êtes avocat ?
-                Vous êtes avocat ?
-                Vous êtes avocat ?
                </Link>
