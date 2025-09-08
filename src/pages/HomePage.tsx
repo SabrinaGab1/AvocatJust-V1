82094d1840242807e3d3ea22db9ebf36a5e2de73
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:50:08 +0200

    Changer le texte du bouton

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,6 @@
                  to="/signup"
 -                className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
 +                className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
-               >
+                Vous êtes avocat ?
                 Vous êtes avocat ?
                </Link>
