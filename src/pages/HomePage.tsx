32ad50b907163bc6dc22912599ca98f15f9e3bfc
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:34:55 +0200

    Ajouter un nouveau bouton Vous êtes avocat

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -159,6 +159,12 @@ export default function HomePage() {
                   {t('login')}
                 </Link>
                 <Link 
+                  to="/signup" 
+                  className="text-gray-700 hover:text-orange-500 transition-colors"
+                >
+                  {t('create_account')}
+                </Link>
+                <Link 
                   to="/signup"
                   className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
                 >
