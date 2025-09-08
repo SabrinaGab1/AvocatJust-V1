20c98f0021c2d6da4f3e18f3a4fb36d8e526228a
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:19:48 +0200

    Changer le bouton créer un compte

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+@@ .. @@
+               <Link 
+                 to="/signup"
+-                className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
++                className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
+               >
+-                Créer un compte
++                Vous êtes avocat ?
+               </Link>
