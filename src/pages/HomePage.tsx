ee0c0f12be0352531e4d76297da44056e7eeb55b
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 14:54:23 +0200

    Modifier le nom du bouton dans le header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -74,7 +74,7 @@ const HomePage = () => {
                 to="/signup"
                 className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
               >
-                Je suis un avocat
+                Vous êtes avocat ?
               </Link>
             </div>
           </div>
