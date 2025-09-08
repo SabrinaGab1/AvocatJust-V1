8519c9cdeb50947fa605ec71f61cec99271b7602
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 15:01:56 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -306,7 +306,7 @@ function HomePage() {
                   onClick={() => navigate('/signup')}
                   className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
                 >
-                  {language === 'fr' ? 'Créer un compte' : 'Create account'}
+                  {language === 'fr' ? 'Vous êtes avocat ?' : 'Create account'}
                 </button>
               </div>
             </div>
