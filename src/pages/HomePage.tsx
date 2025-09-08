79df5fab20390e1d7240b38aebac0cf38677f647
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:36:54 +0200

    Ajouter correctement le bouton avocat

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -166,6 +166,12 @@ export default function HomePage() {
                 </Link>
                 <Link 
                   to="/signup"
+                  className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition-colors"
+                >
+                  Vous êtes avocat ?
+                </Link>
+                <Link 
+                  to="/signup"
                   className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
                 >
                   Vous êtes avocat ?
