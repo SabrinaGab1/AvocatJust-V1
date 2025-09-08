dbf61cb08ff218ef208fdd797d739cc89257572d
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 09:01:10 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,5 @@
 +                className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
                >
 -                Créer un compte
-                Vous êtes avocat ?
 +                Vous êtes avocat ?
                </Link>
