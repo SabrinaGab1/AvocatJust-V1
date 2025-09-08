89c0a6993765e80c3a9e8059c7d4c637ac002bfd
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:20:43 +0200

    Changer seulement le bouton de navigation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,5 +5,6 @@
 +                className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
                >
 -                Créer un compte
+                Vous êtes avocat ?
 +                Vous êtes avocat ?
                </Link>
