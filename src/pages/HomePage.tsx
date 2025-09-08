3974f2ba28b59691b39131c2da529eef9d911032
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:51:55 +0200

    Ajouter un bouton pour rediriger vers l'inscription avocat

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,3 +6,10 @@
                 Vous êtes avocat ?
                 Vous êtes avocat ?
                </Link>
+              
+              <Link 
+                to="/signup"
+                className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition-colors"
+              >
+                Rejoindre AvocaJust
+              </Link>
