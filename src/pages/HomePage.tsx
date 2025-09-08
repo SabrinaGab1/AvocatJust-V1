61abcf52d4a2e15f1a4b0a32fda72641d993e5be
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 08:26:25 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
-@@ .. @@
-               <Link 
-                 to="/signup"
-                 className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
-               >
-                Vous êtes avocat ?
-               </Link>
+<Link 
+  to="/login" 
+  className="text-gray-700 hover:text-orange-500 transition-colors"
+>
+  {t('login')}
+</Link>
+<Link 
+  to="/signup"
+  className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
+>
+ Vous êtes avocat ?
+</Link>
