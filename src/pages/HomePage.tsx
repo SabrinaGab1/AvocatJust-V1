33d36003c7321ef762233b1aa05d40e667ca54c6
SabrinaGab1 <sabrina.gabteni@hotmail.fr>
2025-09-08 09:09:44 +0200

    Changer le texte du bouton

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+@@ .. @@
+                <Link 
+                  to="/signup"
+                  className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition-colors"
+                >
+-                  {t('create_account')}
++                  Vous êtes avocat ?
+                </Link>
